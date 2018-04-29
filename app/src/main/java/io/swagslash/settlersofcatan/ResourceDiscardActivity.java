9983ffe3ea87def1401f30ffcd56e790bb28826f
Thraunig <thraunig@edu.aau.at>
2018-04-29 13:05:33 +0200

    GUI Komplett!
    Relevante Datein sind nur die activity_resource... und die ResourceDiscardActivity.java. Rest soll nicht gemerged werden.
    
    Auswahl der abzuwerfenden Ressourcen möglich
    Coundown 25 Sekunden, dann wird zufällig abgeworfen (Wahrscheinlichkeit gleich groß wie in der Wirklichkeit!)
    Zurücksetzen möglich
    Toast wenn zu wenig oder zu viele Karten ausgewählt wurden bei Confirm
    
    Disabeln der unbenötigten Buttons nach Ablauf des Timers
    Anzeige der Auswahlkarten + noch abzuwerfenden
    
    Java-Doc Vorhanden
    
    Unit-Test sind nicht sinnvoll, da nur zufällig gewählt,Daten aus- bzw. Eingegeben werden (GUI) + Countdown Testen hat keinen Sinn.
    
    Noch zum Tun:
     Aus dem Player Inventar die Anzahl der Ressourcenkarten auslesen (Idee schon auskommentiert programmiert), derzeit werden voreingestellte Werte für die Anzahl der Ressourcenkarte verwendet.
    
     Und das Abwerfen der Karten in der Methode Confirm.
    
     LG

--- app/src/main/java/io/swagslash/settlersofcatan/ResourceDiscardActivity.java
+++ app/src/main/java/io/swagslash/settlersofcatan/ResourceDiscardActivity.java
@@ -1,5 +1,6 @@
 package io.swagslash.settlersofcatan;
 
+import android.graphics.Color;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,35 +8,41 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
+
 public class ResourceDiscardActivity extends AppCompatActivity {
     public int timer = 10;
-    public int erz,holz,getreide, wolle,lehm,gesamtAbwurfkarten = 0;
+    public int erz;
+    public int holz;
+    public int getreide;
+    public int wolle;
+    public int lehm;
+
+    //maxResource hängt von den Handkarten ab. Wenn es funktioniert: Vorher alle auf 0 setzen!!
     int maxHolz =4;
     int maxWolle = 5;
     int maxErz = 3;
     int maxLehm = 1;
     int maxGetreide = 2;
-    int sumResourceToDiscard, countResourceToDiscard;
-    TextView textViewGetreide, textViewLehm, textViewHolz,textViewWolle,textViewErz,textViewTimer,textViewAnweisung,textViewNoch ;
+    int sumResourceInHand, countResourceToDiscard;
+    TextView textViewGetreide, textViewLehm, textViewHolz,textViewWolle,textViewErz,textViewTimer,textViewAnweisung,textViewNoch;
+    Button btnGetreide,btnLehm,btnHolz,btnErz,btnWolle,btnClear,btnConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resource_discard);
 
-        //Bei Wedes Resourcen
-        //WOOD, GRAIN, WOOL, ORE, BRICK
-
-        Button btnGetreide = (Button) findViewById(R.id.buttonGetreide);
-        Button btnLehm =(Button) findViewById(R.id.buttonLehm);
-        Button btnHolz = (Button)findViewById(R.id.buttonHolz);
-        Button btnErz = (Button)findViewById(R.id.buttonErz);
-        Button btnWolle = (Button)findViewById(R.id.buttonWolle);
-        Button btnClear = (Button)findViewById(R.id.buttonClear);
-        Button btnConfirm = (Button)findViewById(R.id.buttonConfirm);
+         btnGetreide = (Button) findViewById(R.id.buttonGetreide);
+         btnLehm =(Button) findViewById(R.id.buttonLehm);
+         btnHolz = (Button)findViewById(R.id.buttonHolz);
+         btnErz = (Button)findViewById(R.id.buttonErz);
+         btnWolle = (Button)findViewById(R.id.buttonWolle);
+         btnClear = (Button)findViewById(R.id.buttonClear);
+         btnConfirm = (Button)findViewById(R.id.buttonConfirm);
         textViewGetreide = (TextView)findViewById(R.id.textViewGetreide);
         textViewGetreide.setText("0/" + maxGetreide);
         textViewLehm = (TextView) findViewById(R.id.textViewLehm);
@@ -48,91 +55,137 @@ public class ResourceDiscardActivity extends AppCompatActivity {
         textViewErz.setText("0/" + maxErz);
         textViewTimer =(TextView) findViewById(R.id.textViewtimer);
         textViewNoch = (TextView) findViewById(R.id.textViewNochNumber);
-
         textViewAnweisung = (TextView) findViewById(R.id.textViewTitel);
 
-        sumResourceToDiscard = maxGetreide+maxErz+maxHolz+maxLehm+maxWolle;
-        countResourceToDiscard = sumResourceToDiscard/2;
+        /* Inventar durchlaufen, um festzustellen welche Resourcen wie oft vorhanden sind.
+        //Player sollte mitgegeben werden
+        Player dran = new Player(null,1, Player.Color.RED,"istdran");
+
+        Resource r;
+         HashMap.Entry entry;
+        Iterator it  = dran.getInventory().getResourceHand().entrySet().iterator();
+           while(it.hasNext()){
+            entry = (Map.Entry) it.next();
+            r = (Resource) entry.getValue();
+            if(r.getResourceType() == Resource.ResourceType.WOOD){
+                maxHolz++;
+            }
+            else if (r.getResourceType() == Resource.ResourceType.BRICK){
+                maxLehm++;
+            }
+            else if (r.getResourceType() == Resource.ResourceType.GRAIN){
+                maxGetreide++;
+            }
+            else if (r.getResourceType() == Resource.ResourceType.ORE){
+                maxErz++;
+            }
+            else if (r.getResourceType() == Resource.ResourceType.WOOL){
+                maxWolle++;
+            }
+
+        }
+        */
+
+
+        //Gesamtanzahk der Handkarten berechnen
+        sumResourceInHand = maxGetreide+maxErz+maxHolz+maxLehm+maxWolle;
+
+        //Gesamtanzahl der Abzuwerfenden Karten berechnen
+        countResourceToDiscard = sumResourceInHand /2;
         textViewNoch.setText(Integer.toString(countResourceToDiscard));
-        textViewAnweisung.setText("Sie haben "+ sumResourceToDiscard +" Ressourcenkarten, werfen sie somit "+ (sumResourceToDiscard /2) +" ab!");
-
-        //TO-DO_ Holen aus RessourcenInventar!
+        textViewAnweisung.setText("Sie haben "+ Integer.toString(sumResourceInHand) +" Ressourcenkarten, werfen sie somit "+ Integer.toString((sumResourceInHand /2)) +" ab!");
 
 
-        //Starte Download
-        countdown10to0();
+        //Start Countdown
+        countdown25to0();
 
+
+        /**
+         * Reset Button. Sets the choosen numbers to Default and displays them.
+         */
         btnClear.setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View v) {
-                       lehm = 0;erz = 0;holz = 0;getreide = 0;wolle =0;gesamtAbwurfkarten=0;
-                       countResourceToDiscard = sumResourceToDiscard/2;
-                       textViewErz.setText(Integer.toString(erz));
-                       textViewGetreide.setText(Integer.toString(getreide));
-                       textViewHolz.setText(Integer.toString(holz));
-                       textViewLehm.setText(Integer.toString(lehm));
-                       textViewWolle.setText(Integer.toString(wolle));
-                       countResourceToDiscard = sumResourceToDiscard/2;
-                       textViewNoch.setText(Integer.toString(countResourceToDiscard));
+                       lehm = 0;erz = 0;holz = 0;getreide = 0;wolle =0;
+                        countResourceToDiscard = sumResourceInHand /2;
+                       textViewErz.setText(Integer.toString(erz) + "/" + Integer.toString(maxErz));
+                       textViewGetreide.setText(Integer.toString(getreide)+ "/" + Integer.toString(maxGetreide));
+                       textViewHolz.setText(Integer.toString(holz)+ "/" + Integer.toString(maxHolz));
+                       textViewLehm.setText(Integer.toString(lehm)+ "/" + Integer.toString(maxLehm));
+                       textViewWolle.setText(Integer.toString(wolle)+ "/" + Integer.toString(maxWolle));
+                       printCountToDiscard();;
                     }
                 }
         );
-
+        /**
+         * Increase Ore Button
+         */
         btnErz.setOnClickListener(
             new View.OnClickListener() {
                 public void onClick(View v) {if(erz< maxErz){
-                    erz++;gesamtAbwurfkarten++;
+                    erz++;
                     textViewErz.setText(Integer.toString(erz)+"/" +Integer.toString(maxErz));
 
                     countResourceToDiscard--;
-                    textViewNoch.setText(Integer.toString(countResourceToDiscard));
+                    printCountToDiscard();
                 }}
         });
+        /**
+         * Increase Grain Button
+         */
         btnGetreide.setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View v) {
                         if(getreide < maxGetreide){
-                        getreide++;gesamtAbwurfkarten++;
+                        getreide++;
                         textViewGetreide.setText(Integer.toString(getreide)+"/" +Integer.toString(maxGetreide));
                             countResourceToDiscard--;
-                            textViewNoch.setText(Integer.toString(countResourceToDiscard));
+                            printCountToDiscard();
                     }}
                 });
+        /**
+         * Increase Brick Button
+         */
         btnLehm.setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View v) {if(lehm < maxLehm){
-                                               lehm++;gesamtAbwurfkarten++;
+                                               lehm++;
                         textViewLehm.setText(Integer.toString(lehm) +"/" +Integer.toString(maxLehm));
                         countResourceToDiscard--;
-
-                        textViewNoch.setText(Integer.toString(countResourceToDiscard));
+                        printCountToDiscard();
                     }}
                 });
+        /**
+         * Increase Wool Button
+         */
         btnWolle.setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View v) {
                         if(wolle < maxWolle){
-                        wolle++;gesamtAbwurfkarten++;
+                        wolle++;
                         textViewWolle.setText(Integer.toString(wolle) +"/" +Integer.toString(maxWolle));
                         countResourceToDiscard--;
-
-                            textViewNoch.setText(Integer.toString(countResourceToDiscard));
+                            printCountToDiscard();
                     }}
                 });
-
+        /**
+         * Increase Wood Button
+         */
         btnHolz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(holz < maxHolz){
                 holz++;
-                    gesamtAbwurfkarten++;
                 textViewHolz.setText(Integer.toString(holz)+"/" +Integer.toString(maxHolz));
                     countResourceToDiscard--;
+                    printCountToDiscard();
 
-                    textViewNoch.setText(Integer.toString(countResourceToDiscard));
             }}
         });
 
+        /**
+         * Confirm Button
+         */
         btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -142,25 +195,48 @@ public class ResourceDiscardActivity extends AppCompatActivity {
         });
     }
 
-    //Bestätigung des Abwurfes und zurück
-    public void confirmDiscard(){
-
+    public void printCountToDiscard(){
+        textViewNoch.setText(Integer.toString(countResourceToDiscard));
     }
 
 
+    /**
+     * Confirm the choosen Discard. Removes discarded Resources from
+     * the Resource Hand
+     */
+    public void confirmDiscard(){
+
+        if(wolle+getreide+lehm+erz+holz!= (int) (sumResourceInHand/2)){
+            Toast toast = Toast.makeText(getApplicationContext(), "Zu viele oder zu wenige Karten ausgewählt!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        else{
+
+        }
+
+    // TO-DO Remove Cards from Players Hand
+
+    }
+
+    /**
+     * Choose Random Cards to discard from Hand after Timeout.
+     * Probability depends on how many cards per Resource Type are
+     * on the Hand (=Real Probability
+     *
+     */
     public void randomDiscard(){
         getreide = 0;lehm = 0;holz=0;erz=0;wolle = 0;
-        int numberDiscard = sumResourceToDiscard / 2;
-        //Reihenfolge 0-maxGetreide,maxHolz-maxHolz
+        int numberDiscard = (int) sumResourceInHand / 2;
+        //Schranken
         int s1 = maxGetreide;
-        int s2 = s1+maxLehm;
+        int s2 = s1+ maxLehm;
         int s3 = s2 + maxHolz;
         int s4 = s3 + maxWolle;
         Random R = new Random();
-
+        int rand;
         //zufällig numberDiscard erzeugen
-        for(int i = 0; i < numberDiscard;i++){
-            int rand = R.nextInt(sumResourceToDiscard);
+        for(int i = 1; i <= numberDiscard;i++){
+            rand = R.nextInt(sumResourceInHand);
             if(rand <s1){
                 //Discard Getreide
                 if(getreide<maxGetreide) {
@@ -186,7 +262,8 @@ public class ResourceDiscardActivity extends AppCompatActivity {
             }
             else if(rand < s4){
                 //Discard Wolle
-                if(wolle<maxWolle){wolle++;}
+                if(wolle<maxWolle){
+                    wolle++;}
                 else{
                     i--;
                 }
@@ -200,22 +277,40 @@ public class ResourceDiscardActivity extends AppCompatActivity {
                 }
             }
         }
-        Log.v("Resource Discard", "Random Discard confirmed");
-        //Bestätigen des Abwurfes
-        confirmDiscard();
+
+        textViewGetreide.setText(Integer.toString(getreide) + "/" +Integer.toString(maxGetreide));
+        textViewErz.setText(Integer.toString(erz) + "/" +Integer.toString(maxErz));
+        textViewLehm.setText(Integer.toString(lehm) + "/" +Integer.toString(maxLehm));
+        textViewWolle.setText(Integer.toString(wolle) + "/" +Integer.toString(maxWolle));
+        textViewHolz.setText(Integer.toString(holz) + "/" +Integer.toString(maxHolz));
+        textViewNoch.setText("0");
+        btnClear.setEnabled(false);
+        btnWolle.setEnabled(false);
+        btnGetreide.setEnabled(false);
+        btnErz.setEnabled(false);
+        btnHolz.setEnabled(false);
+        btnLehm.setEnabled(false);
+        btnConfirm.setBackgroundColor(Color.GREEN);
+
+
+        Log.v("Resource Discard", "Random Discard confirmed" + " Holz " + holz + " Getreide " + getreide
+        +" Erz " + erz + " Wolle " + wolle + " Lehm "+ getDelegate());
 
     }
 
 
-    //Countdown bis zum zufälligen Abwurf
-    public void countdown10to0(){
+    /**
+     * After 25 Seconds, the Coundown finished and the Resource will choose randomly
+     *
+     */
+    public void countdown25to0(){
         new CountDownTimer(25000, 1000){
             public void onTick(long millisUntilFinished) {
                   textViewTimer.setText(Long.toString(millisUntilFinished / 1000));
             }
 
             public void onFinish() {
-                textViewTimer.setText("Zeit vorbei!");
+                textViewTimer.setText("Zeit vorbei! Zufällige Auswahl!");
                 randomDiscard();
 
             }
