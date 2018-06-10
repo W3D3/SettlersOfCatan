23a3c437a1de9c8c98a8f8a6b89bdc4ae537340b
wiphadaba <wiphadaba@edu.aau.at>
2018-06-10 13:15:49 +0200

    change Bank and BankTest

--- app/src/main/java/io/swagslash/settlersofcatan/pieces/items/Bank.java
+++ app/src/main/java/io/swagslash/settlersofcatan/pieces/items/Bank.java
@@ -12,15 +12,10 @@ public class Bank implements IBank {
 
     @Override
     public boolean payForStreet(Player player) {
-        // get the hand of the player
-        Inventory inventory = player.getInventory();
+        if (canPayForStreet(player)) {
+            // get the hand of the player
+            Inventory inventory = player.getInventory();
 
-        // count how many bricks and wood the player has on his hand
-        int countBrick = inventory.countResource(brick.getResourceType());
-        int countWood = inventory.countResource(wood.getResourceType());
-
-        // the player needs 1 brick and 1 wood
-        if ((countBrick >= 1) && (countWood >= 1)) {
             // remove from hand
             inventory.removeResource(brick);
             inventory.removeResource(wood);
@@ -30,22 +25,16 @@ public class Bank implements IBank {
         } else {
             // not enough resources to build a street
             return false;
+
         }
     }
 
     @Override
     public boolean payForSettlement(Player player) {
-        // get the hand of the player
-        Inventory inventory = player.getInventory();
+        if (canPayForSettlement(player)) {
+            // get the hand of the player
+            Inventory inventory = player.getInventory();
 
-        // count how many bricks, wood, grains and wools the player has on his hand
-        int countBrick = inventory.countResource(brick.getResourceType());
-        int countWood = inventory.countResource(wood.getResourceType());
-        int countGrain = inventory.countResource(grain.getResourceType());
-        int countWool = inventory.countResource(wool.getResourceType());
-
-        // the player needs 1 brick, 1 wood, 1 grain, 1 wool
-        if ((countBrick >= 1) && (countWood >= 1) && (countGrain >= 1) && (countWool >= 1)) {
             // remove from hand
             inventory.removeResource(brick);
             inventory.removeResource(wood);
@@ -62,16 +51,10 @@ public class Bank implements IBank {
 
     @Override
     public boolean payForCity(Player player) {
-        // get the hand of the player
-        Inventory inventory = player.getInventory();
+        if (canPayForCity(player)) {
+            // get the hand of the player
+            Inventory inventory = player.getInventory();
 
-        // count how many grains and ores the player has on his hand
-        int countGrain = inventory.countResource(grain.getResourceType());
-        int countOre = inventory.countResource(ore.getResourceType());
-
-
-        // the player needs 2 grains and 3 ores
-        if ((countGrain >= 2) && (countOre >= 3)) {
             // remove from hand
             for (int i = 0; i < 3; i++) {
                 if (i < 2) {
@@ -90,6 +73,91 @@ public class Bank implements IBank {
 
     @Override
     public boolean payForCard(Player player) {
+        if (canPayForCard(player)) {
+            // get the hand of the player
+            Inventory inventory = player.getInventory();
+
+            // remove from hand
+            inventory.removeResource(wool);
+            inventory.removeResource(grain);
+            inventory.removeResource(ore);
+
+            // resources have been paid
+            return true;
+        } else {
+            // not enough resources to buy a development card
+            return false;
+        }
+    }
+
+
+    /**
+     * Check if you can pay for something without removing resources from hand
+     *
+     * @return true if you have enough resources to pay
+     */
+
+    public boolean canPayForStreet(Player player) {
+        // get the hand of the player
+        Inventory inventory = player.getInventory();
+
+        // count how many bricks and wood the player has on his hand
+        int countBrick = inventory.countResource(brick.getResourceType());
+        int countWood = inventory.countResource(wood.getResourceType());
+
+        // the player needs 1 brick and 1 wood
+        if ((countBrick >= 1) && (countWood >= 1)) {
+            // player can pay
+            return true;
+        } else {
+            // not enough resources to pay
+            return false;
+        }
+    }
+
+
+    public boolean canPayForSettlement(Player player) {
+        // get the hand of the player
+        Inventory inventory = player.getInventory();
+
+        // count how many bricks, wood, grains and wools the player has on his hand
+        int countBrick = inventory.countResource(brick.getResourceType());
+        int countWood = inventory.countResource(wood.getResourceType());
+        int countGrain = inventory.countResource(grain.getResourceType());
+        int countWool = inventory.countResource(wool.getResourceType());
+
+        // the player needs 1 brick, 1 wood, 1 grain, 1 wool
+        if ((countBrick >= 1) && (countWood >= 1) && (countGrain >= 1) && (countWool >= 1)) {
+            // player can pay
+            return true;
+        } else {
+            // not enough resources to pay
+            return false;
+        }
+    }
+
+
+    public boolean canPayForCity(Player player) {
+        // get the hand of the player
+        Inventory inventory = player.getInventory();
+
+        // count how many grains and ores the player has on his hand
+        int countGrain = inventory.countResource(grain.getResourceType());
+        int countOre = inventory.countResource(ore.getResourceType());
+
+
+        // the player needs 2 grains and 3 ores
+        if ((countGrain >= 2) && (countOre >= 3)) {
+            // player can pay
+            return true;
+        } else {
+            // not enough resources to pay
+            return false;
+        }
+    }
+
+
+    public boolean canPayForCard(Player player) {
         // get the hand of the player
         Inventory inventory = player.getInventory();
 
@@ -100,15 +168,10 @@ public class Bank implements IBank {
 
         // the player needs 1 wool, 1 grain and 1 ore
         if ((countWool >= 1) && (countGrain >= 1) && (countOre >= 1)) {
-            // remove from hand
-            inventory.removeResource(wool);
-            inventory.removeResource(grain);
-            inventory.removeResource(ore);
-
-            // resources have been paid
+            // player can pay
             return true;
         } else {
-            // not enough resources to buy a development card
+            // not enough resources to pay
             return false;
         }
     }
