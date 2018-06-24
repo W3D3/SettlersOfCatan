43aab2858e0a2325378c91781013fcad40e98045
creatio_ex_nihilo@x230 <dominicmicpa@edu.aau.at>
2018-06-24 22:34:32 +0200

    updated ShakeTest

--- app/src/test/java/io/swagslash/settlersofcatan/ShakeTest.java
+++ app/src/test/java/io/swagslash/settlersofcatan/ShakeTest.java
@@ -1,6 +1,5 @@
 package io.swagslash.settlersofcatan;
 
-import android.app.Application;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -9,74 +8,81 @@ import android.hardware.SensorManager;
 import junit.framework.Assert;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
-import org.robolectric.shadow.api.Shadow;
 import org.robolectric.shadows.ShadowSensorManager;
+
+import java.lang.reflect.Field;
 
 import static org.robolectric.Shadows.shadowOf;
 
-@Ignore
 @RunWith(RobolectricTestRunner.class)
 public class ShakeTest {
 
-    private String shouldSay = "test";
-    private static final int VAL = 42;
-    private SensorEvent se;
+    private Sensor s;
+    private ShadowSensorManager ssm;
     private ShakeDetector sd;
-    private ShakeListener sl;
-    private int testValue = 0;
+
+    private String shouldSay = "test";
+    private String testValue;
+
+    private float[] sensorVals = {5.2f, 4.2f, -14.2f};
 
     @Before
     public void init() {
-        /*
-        se = Mockito.mock(SensorEvent.class);
-        try {
-            Field vals = SensorEvent.class.getField("values");
-            vals.setAccessible(true);
-            float[] sensorVals = {5.2f, 4.2f, -14.2f};
-            try {
-                vals.set(se, sensorVals);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        }
         sd = new ShakeDetector();
-        sd.setShakeListener(new ShakeListener() {
+        ShakeListener sl = new ShakeListener() {
             @Override
             public void onShake() {
-                System.out.println("test");
-                testValue = VAL;
+                testValue = shouldSay;
             }
-        });
-        */
+        };
+        sd.setShakeListener(sl);
 
-        sd = new ShakeDetector();
-        sd.setShakeListener(new ShakeListener() {
-            @Override
-            public void onShake() {
-                System.out.println(shouldSay);
-                testValue = VAL;
-            }
-        });
-
-        Application app = RuntimeEnvironment.application;
-        SensorManager sm = (SensorManager) app.getSystemService(Context.SENSOR_SERVICE);
-        ShadowSensorManager ssm = shadowOf(sm);
-        Sensor s = Shadow.newInstanceOf(Sensor.class);
-        ssm.addSensor(Sensor.TYPE_ACCELEROMETER, s);
-        ssm.registerListener(sd, s, 1);
+        SensorManager sm = (SensorManager) RuntimeEnvironment.application.getSystemService(Context.SENSOR_SERVICE);
+        ssm = shadowOf(sm);
+        s = ssm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        ssm.registerListener(sd, s, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Test
-    @Ignore
+    public void hasListener() {
+        Assert.assertTrue(ssm.hasListener(sd));
+    }
+
+    @Test
     public void checkShakeListener() {
-        sd.onSensorChanged(se);
-        Assert.assertSame(VAL, testValue);
+        try {
+            sd.onSensorChanged(getAccelerometerEventWithValues(s, sensorVals));
+            Assert.assertSame(shouldSay, testValue);
+            // just for the coverage
+            sd.onAccuracyChanged(s, 1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * taken from
+     * https://stackoverflow.com/questions/34530865/how-to-mock-motionevent-and-sensorevent-for-unit-testing-in-android
+     *
+     * @param s             Sensor to read SensorEvent from
+     * @param desiredValues Values to read
+     */
+    private SensorEvent getAccelerometerEventWithValues(Sensor s, float[] desiredValues) throws Exception {
+        SensorEvent sensorEvent = Mockito.mock(SensorEvent.class);
+
+        Field sensorField = SensorEvent.class.getField("sensor");
+        sensorField.setAccessible(true);
+        sensorField.set(sensorEvent, s);
+
+        Field valuesField = SensorEvent.class.getField("values");
+        valuesField.setAccessible(true);
+        valuesField.set(sensorEvent, desiredValues);
+
+        return sensorEvent;
     }
 }
